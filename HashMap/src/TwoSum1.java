ddf794f366a909055f3c68ecd69a89f5615a0b78
V3DxNT <ved4geeks@gmail.com>
2025-08-02 09:07:28 +0530

    Solved TwoSum (LeetCode:1) using HashMaps

---
+++ HashMap/src/TwoSum1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class TwoSum1 {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int complement=target-nums[i];
+            if(map.containsKey(complement))
+            {
+                return new int[]{i,map.get(complement)};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{};
+    }
+
+}
