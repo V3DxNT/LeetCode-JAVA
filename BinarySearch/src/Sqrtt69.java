fbe2c07564379e5e760e9a67c358dd5fcb1a43eb
V3DxNT <ved4geeks@gmail.com>
2025-08-06 16:47:57 +0530

    Solve SquareRoot LC:69

--- BinarySearch/src/Sqrtt69.java
+++ BinarySearch/src/Sqrtt69.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Sqrtt69 {
+    public static void main(String[] args) {
+
+    }
+    public int mySqrt(int x) {
+        int s=0;
+        int e=x;
+        if(x<=1) return x;
+        while(s<=e)
+        {
+            int mid=(int) (s+e)/2;
+            long sq=(long)mid*mid;
+            if(sq == x) return mid;
+
+            else if(sq>x) e=mid-1;
+            else s=mid+1;
+        }
+        return e;
+
+    }
 }
