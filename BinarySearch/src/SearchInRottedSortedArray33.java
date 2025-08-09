4f4bc43179ea8c124ffb87b4e52c89fce3be5266
V3DxNT <ved4geeks@gmail.com>
2025-08-09 16:18:45 +0530

    Search in Rotated Sorted Array LC:33

--- BinarySearch/src/SearchInRottedSortedArray33.java
+++ BinarySearch/src/SearchInRottedSortedArray33.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class SearchInRottedSortedArray33 {
+    public static void main(String[] args) {
+
+    }
+    public int search(int[] nums, int target) {
+        return helper(nums,target,0,nums.length-1);
+    }
+
+    static int helper(int[] arr,int target,int s,int e){
+        if(s>e) return -1;
+        int m=s+(e-s)/2;
+        if(arr[m]== target) return m ;
+
+        if(arr[s] <= arr[m])
+        {
+            if(target>=arr [s] && target<=arr[m])
+            {
+                return helper(arr,target,s,m-1);
+            }
+            else return helper(arr,target,m+1,e);
+        }
+
+        if(target >= arr[m] && target<=arr[e])
+        {
+            return helper(arr,target,m+1,e);
+        }
+        else  return helper(arr,target,s,m-1);
+    }
 }
