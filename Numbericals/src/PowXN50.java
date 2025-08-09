829af06678fe7c107d6f776415a655c7c4b6d1d3
V3DxNT <ved4geeks@gmail.com>
2025-08-09 22:42:36 +0530

    Solve Power x,n LC:50

--- Numbericals/src/PowXN50.java
+++ Numbericals/src/PowXN50.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class PowXN50 {
+    public double myPow(double x, int n) {
+        double pow=1;
+        if(x==1) return 1;
+        if(x==(-1) && n==Integer.MIN_VALUE) return 1;
+        if(x==(-1)) return -1;
+
+        if(n== Integer.MIN_VALUE) return 0;
+
+        double N=Math.abs(n);
+
+        for(int i=0;i<N;i++)
+        {
+            pow=pow*x;
+        }
+
+        if(n >0){
+            return pow;
+        }else{
+            return (1/pow);
+        }
+    }
 }
