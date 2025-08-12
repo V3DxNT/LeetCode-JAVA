52657a59fe49ff4fd877b73ffb554a5e2f04098a
V3DxNT <ved4geeks@gmail.com>
2025-08-12 13:19:29 +0530

    Solve Longest Consequtive Sequence LC:128

--- Array/src/LongestConsequtiveSequence128.java
+++ Array/src/LongestConsequtiveSequence128.java
@@ -1,4 +1,32 @@
 package PACKAGE_NAME;
 
 public class LongestConsequtiveSequence128 {
+
+        public int longestConsecutive(int[] nums) {
+            if(nums.length ==0) return 0;
+
+            HashSet<Integer> list= new HashSet<>();
+            for(int i =0;i<nums.length;i++){
+                list.add(nums[i]);
+            }
+
+            int cCount=1;
+            int largest=1;
+
+            int seqStart;
+            for(int i:list){
+
+                if(!list.contains(i-1)){
+                    cCount=1;
+                    seqStart=i;
+                    while(list.contains(seqStart+1)){
+                        cCount++;
+                        seqStart++;
+                    }
+                    largest=Math.max(largest,cCount);
+                }
+            }
+            return largest;
+        }
+
 }
