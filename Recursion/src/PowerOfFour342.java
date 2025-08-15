ea319a8102873697362c106d7aa35e7123f1b1f9
V3DxNT <ved4geeks@gmail.com>
2025-08-15 11:11:59 +0530

    Solve Power Of Four LC:342

--- Recursion/src/PowerOfFour342.java
+++ Recursion/src/PowerOfFour342.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class PowerOfFour342 {
+    public boolean isPowerOfFour(int n) {
+        return helper(n,0);
+    }
+
+    static boolean helper(int n,int expo){
+        if(n < Math.pow(4,expo)) return false;
+
+        if(n == Math.pow(4,expo)) return  true;
+
+        return helper(n,expo+1);
+    }
 }
